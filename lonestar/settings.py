41ec01203a45aa3100362b217539516e9dd4bc3a
Jasper Otieno <jasperotieno1@gmail.com>
2025-10-19 17:43:21 +0300

    Prepare for deployment

--- lonestar/settings.py
+++ lonestar/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jasper123!@#random_secure_key_here"
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
